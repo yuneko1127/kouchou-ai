f1f8482d3b7d2ee70164ffe0a16ee72baf8d77a0
なのくろ <nanocloudx@gmail.com>
2025-02-20 17:49:06 +0900

    initialize

--- client-admin/README.md
+++ client-admin/README.md
@@ -16,4 +16,7 @@ npm start
   - shotokutaishi-server が動作するエンドポイント
 - `NEXT_PUBLIC_API_KEY`
   - shotokutaishi-server の管理向けAPIを利用するためのAPIキー
- 
+- `BASIC_AUTH_USERNAME`
+  - basic 認証のユーザー名 (空欄の場合は認証スキップ)
+- `BASIC_AUTH_PASSWORD`
+  - basic 認証のパスワード (空欄の場合は認証スキップ)
---
+++ client-admin/middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  if (
+    process.env.BASIC_AUTH_USERNAME === undefined ||
+    process.env.BASIC_AUTH_PASSWORD === undefined
+  ) {
+    return NextResponse.next()
+  }
+  const basicAuth = req.headers.get('authorization')
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [username, password] = Buffer.from(authValue, 'base64').toString().split(':')
+    if (
+      username === process.env.BASIC_AUTH_USERNAME &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next()
+    }
+  }
+  return NextResponse.json(
+    { error: 'Basic Auth Required' },
+    {
+      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+      status: 401,
+    }
+  )
+}
