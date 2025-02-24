b3553a5dc7a96fa04fd63358bdf04e0e77a4edc6
なのくろ <nanocloudx@gmail.com>
2025-02-24 18:18:48 +0900

    rm debug

--- dummy-server/app/admin/reports/route.ts
+++ dummy-server/app/admin/reports/route.ts
@@ -45,7 +45,6 @@ export async function POST(request: Request) {
     })
   }
   const body = await request.json()
-  console.log(body)
   return new Response(null, {
     headers: {
       'Content-Type': 'application/json',
