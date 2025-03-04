669125b704c47ba8a84baeff427b74fd51c057e3
nasuka <nsk.smn@gmail.com>
2025-03-04 17:02:45 +0900

    fix getApiBaseUrl

--- client/app/utils/api.js
+++ client/app/utils/api.js
@@ -2,9 +2,10 @@ export const getApiBaseUrl = () => {
   if (typeof window !== 'undefined') {
     return process.env.NEXT_PUBLIC_API_BASEPATH
   }
-  return process.env.API_BASEPATH
+  // undefinedでなければAPI_BASEPATHを返す
+  if (process.env.API_BASEPATH) {
+    return process.env.API_BASEPATH
+  }
+  return process.env.NEXT_PUBLIC_API_BASEPATH
 }
 
-export const getResourceUrl = (path) => {
-  return `${getApiBaseUrl()}${path}`
-}
