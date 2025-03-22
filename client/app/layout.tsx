9d9534e0778fd0839bba9c7683dc696d6d273cb0
101ta28 <workta28@gmail.com>
2025-03-23 01:40:43 +0900

    日本語化ファイルを読み込むように修正

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,12 +1,22 @@
+'use client'
+
+import { useEffect } from 'react'
 import ClientProvider from './ClientProvider'
 import './global.css'
-import {getApiBaseUrl} from './utils/api'
+import { getApiBaseUrl } from './utils/api'
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = '/js/plotly-locale-ja.js'
+    script.async = true
+    document.body.appendChild(script)
+  }, [])
+
   return (
-    <html suppressHydrationWarning lang={'ja'}>
+    <html suppressHydrationWarning lang='ja'>
       <head>
-        <link rel={'icon'} href={getApiBaseUrl() + '/meta/icon.png'} sizes={'any'}/>
+        <link rel='icon' href={getApiBaseUrl() + '/meta/icon.png'} sizes='any' />
       </head>
       <body>
         <ClientProvider>
