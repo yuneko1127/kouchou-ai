152ff8a1b67d879c054b35c73b047831a7a8ab2e
shgtkshruch <shgtk.shruch@gmail.com>
2025-03-23 15:38:47 +0900

    Add not-found page

---
+++ client/app/[slug]/not-found.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@chakra-ui/react'
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <>
+      <p>ページが見つかりませんでした</p>
+      <Link href="/">
+        <Button>
+          トップに戻る
+        </Button>
+      </Link>
+    </>
+  )
+}
