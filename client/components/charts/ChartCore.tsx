22c1d00640226d7bcf33777ce0c5e50ff35fc8b4
101ta28 <workta28@gmail.com>
2025-03-23 17:22:10 +0900

    日本語化ファイルをimportするように修正

--- client/components/charts/ChartCore.tsx
+++ client/components/charts/ChartCore.tsx
@@ -1,14 +1,23 @@
 'use client'
+
 import dynamic from 'next/dynamic'
 import React from 'react'
-import {LoadingBar} from '@/components/report/LoadingBar'
+import { LoadingBar } from '@/components/report/LoadingBar'
+import jaLocale from '@/lib/plotly-locale-ja'
 
 export const ChartCore = dynamic(async () => {
   const Plotly = await import('plotly.js/lib/core')
   const Scatter = await import('plotly.js/lib/scatter')
   const Sunburst = await import('plotly.js/lib/sunburst')
   const Treemap = await import('plotly.js/lib/treemap')
+
   const createPlotlyComponent = (await import('react-plotly.js/factory')).default
+
   Plotly.register([Scatter, Sunburst, Treemap])
+  Plotly.register(jaLocale)
+
   return createPlotlyComponent(Plotly)
-}, {ssr: false, loading: () => <LoadingBar loaded={100} max={100} isVisualizing/>})
+}, {
+  ssr: false,
+  loading: () => <LoadingBar loaded={100} max={100} isVisualizing />
+})
